1c42e2e7eb3732b2a3324315841453fa337a52d4
hyzen0 <hail4heisenberg@gmail.com>
2021-03-02 18:00:58 +0530

    register

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Config dotev...
+require("dotenv").config({
+  path: "./config/config.env",
+});
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// body parser
+app.use(bodyParser.json());
+
+// Load routes
+const authRouter = require("./api/auth.route.js");
+
+// Dev Logging Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(cors());
+  app.use(morgan("dev"));
+}
+
+// Use Routes
+app.use("/", res => {
+  console.log("working");
+});
+app.use("/api", authRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    msg: "Page not found",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
