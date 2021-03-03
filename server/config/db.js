27eae99a9be9d354ae9bd1d5edaf227b6ad1f921
hyzen0 <hail4heisenberg@gmail.com>
2021-03-03 16:42:45 +0530

    Error: Db

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,13 @@
-const MongoClient = require("mongodb").MongoClient
+const mongoose = require("mongoose");
 
-//Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set,
-//  and specify the read preference as secondary preferred
 const connectDB = async () => {
-  const connection = await MongoClient.connect(process.env.MONGO_URI, {
+  const connection = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   });
+
   console.log(`MongoDB Connected: ${connection.connection.host}`);
 };
 
