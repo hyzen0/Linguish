ce6740153ba9e192249cca5ee1b11a9c90935a4f
hyzen0 <hail4heisenberg@gmail.com>
2021-03-03 16:40:17 +0530

    db

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient
 
+//Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set,
+//  and specify the read preference as secondary preferred
 const connectDB = async () => {
-  const connection = await mongoose.connect(process.env.MONGO_URI, {
+  const connection = await MongoClient.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   });
-
   console.log(`MongoDB Connected: ${connection.connection.host}`);
 };
 
